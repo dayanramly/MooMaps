e9b6f8489a8cd4b083544ff34069353d5960f5f5
dayanramly <dayzramly@gmail.com>
2015-03-21 20:12:34 +0700

    MooMaps Fix

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\xampp\htdocs\MooMaps\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\xampp\htdocs\MooMaps\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\xampp\htdocs\MooMaps\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
